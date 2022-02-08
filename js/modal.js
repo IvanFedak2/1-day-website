1f92fe4df3bd2597f06a0e9b9e5fd05caadc6594
IvanFedak2 <reneicanc@gmail.com>
2022-02-08 23:23:01 +0200

    fixing

--- js/modal.js
+++ js/modal.js
@@ -3,16 +3,15 @@ let btn = document.getElementById("myBtn");
 let span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
+  $(modal).removeClass('out');
   modal.style.display = "block";
 }
 span.onclick = function() {
-  modal.style.display = "none";
+  $(modal).addClass('out');
 }
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
-   
-    
+    $(modal).addClass('out');
   }
 }
 ////////////////////////////////////////////////////////////////////
@@ -22,14 +21,15 @@ let btn2 = document.getElementById("myBtn2");
 let span2 = document.getElementsByClassName("close2")[0];
 
 btn2.onclick = function() {
+  $(modal2).removeClass('out');
   modal2.style.display = "block";
 }
 span2.onclick = function() {
-  modal2.style.display = "none";
+  $(modal2).addClass('out');
 }
 window.onclick = function(event) {
   if (event.target == modal2) {
-    modal2.style.display = "none";
+    $(modal2).addClass('out');
   }
 }
 ////////////////////////////////////////////////////////////////////
@@ -38,14 +38,15 @@ let btn3 = document.getElementById("myBtn3");
 let span3 = document.getElementsByClassName("close3")[0];
 
 btn3.onclick = function() {
+  $(modal3).removeClass('out');
   modal3.style.display = "block";
 }
 span3.onclick = function() {
-  modal3.style.display = "none";
+  $(modal3).addClass('out');
 }
 window.onclick = function(event) {
   if (event.target == modal3) {
-    modal3.style.display = "none";
+    $(modal3).addClass('out');
   }
 }
 ////////////////////////////////////////////////////////////////////
@@ -54,14 +55,15 @@ let btn4 = document.getElementById("myBtn4");
 let span4 = document.getElementsByClassName("close4")[0];
 
 btn4.onclick = function() {
+  $(modal4).removeClass('out');
   modal4.style.display = "block";
 }
 span4.onclick = function() {
-  modal4.style.display = "none";
+  $(modal4).addClass('out');
 }
 window.onclick = function(event) {
   if (event.target == modal4) {
-    modal4.style.display = "none";
+    $(modal4).addClass('out');
   }
 }
 ////////////////////////////////////////////////////////////////////
@@ -70,14 +72,15 @@ let btn5 = document.getElementById("myBtn5");
 let span5 = document.getElementsByClassName("close5")[0];
 
 btn5.onclick = function() {
+  $(modal5).removeClass('out');
   modal5.style.display = "block";
 }
 span5.onclick = function() {
-  modal5.style.display = "none";
+  $(modal5).addClass('out');
 }
 window.onclick = function(event) {
   if (event.target == modal5) {
-    modal5.style.display = "none";
+    $(modal5).addClass('out');
   }
 }
 ////////////////////////////////////////////////////////////////////
@@ -86,14 +89,15 @@ let btn6 = document.getElementById("myBtn6");
 let span6 = document.getElementsByClassName("close6")[0];
 
 btn6.onclick = function() {
+  $(modal6).removeClass('out');
   modal6.style.display = "block";
 }
 span6.onclick = function() {
-  modal6.style.display = "none";
+  $(modal6).addClass('out');
 }
 window.onclick = function(event) {
   if (event.target == modal6) {
-    modal6.style.display = "none";
+    $(modal6).addClass('out');
   }
 }
 ////////////////////////////////////////////////////////////////////
@@ -102,14 +106,15 @@ let btn7 = document.getElementById("myBtn7");
 let span7 = document.getElementsByClassName("close7")[0];
 
 btn7.onclick = function() {
+  $(modal7).removeClass('out');
   modal7.style.display = "block";
 }
 span7.onclick = function() {
-  modal7.style.display = "none";
+  $(modal7).addClass('out');
 }
 window.onclick = function(event) {
   if (event.target == modal7) {
-    modal7.style.display = "none";
+    $(modal7).addClass('out');
   }
 }
 ////////////////////////////////////////////////////////////////////
@@ -118,14 +123,15 @@ let btn8 = document.getElementById("myBtn8");
 let span8 = document.getElementsByClassName("close8")[0];
 
 btn8.onclick = function() {
+  $(modal8).removeClass('out');
   modal8.style.display = "block";
 }
 
 span8.onclick = function() {
-  modal8.style.display = "none";
+  $(modal8).addClass('out');
 }
 window.onclick = function(event) {
   if (event.target == modal8) {
-    modal8.style.display = "none";
+    $(modal8).addClass('out');
   }
 }
