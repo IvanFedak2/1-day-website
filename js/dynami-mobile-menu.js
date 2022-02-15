015b1406b81ea58b1c51a08ac28773aca433f38f
IvanFedak2 <reneicanc@gmail.com>
2022-02-15 21:40:54 +0200

    dynamic

--- js/dynami-mobile-menu.js
+++ js/dynami-mobile-menu.js
@@ -1,51 +1,43 @@
-// const renderMenu = function() {
-// 		document.getElementById("header_navigation").insertAdjacentHTML('afterend',
-// 			`
-//             <span id="disable-scroll" class="close0 close-menu">
-//                 <ul class="header_navigation_items"> 
-//                 <a href="#works" class="modal-menu-nav">About us</a>
-//                 <a href="#our-tours" class="modal-menu-nav">Our tours</a>
-//                 <a href="#footer" class="modal-menu-nav">Contact us</a>
-//                 <a href="#works" class="modal-menu-nav">Discount request</a>
-//                 <a href="#works" class="modal-menu-nav">Hot deals</a>
-//             </ul>
-//               <div class="modal-social-media">
-//                 <a class="btn-modal-social btn-outline-light btn-floating m-1" href="https://www.facebook.com/reneicanc"
-//                     role="button" target="_blank"><i class="fab fa-facebook-f" style="margin-left: 2px;"></i></a>
-//                 <a class="btn-modal-social btn-outline-light btn-floating m-1" href="https://twitter.com/IvanFedak" role="button"
-//                     target="_blank"><i class="fab fa-twitter"></i></a>
-//                 <a class="btn-modal-social btn-outline-light btn-floating m-1" href="google.com" role="button" target="_blank">
-//                     <i class="fab fa-google"></i></a>
-//                 <a class="btn-modal-social btn-outline-light btn-floating m-1" href="https://www.instagram.com/cruise_blog"
-//                     role="button" target="_blank"><i class="fab fa-instagram"></i></a>
-//                 <a class="btn-modal-social btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/ivanfedak"
-//                     role="button" target="_blank"><i class="fab fa-linkedin-in"></i></a>
-//                 <a class="btn-modal-social btn-outline-light btn-floating m-1" href="https://github.com/IvanFedak2" role="button"
-//                     target="_blank"><i class="fab fa-github"></i></a>
-//                 </div>
-//             </span>
-// 		`);
-// 	}
-//     renderMenu()
-
-
-
-// const resize = function(){
-
-//     (window).bind("resize", function(){
-//         console.log($(this).width())
-//         if($(this).width() < 800){
-//         document.getElementById('carousel').removeClass('carousel').addClass('')
-//         }
-//     })
-//     };
-
-//     resize()
-// const resize = function(){
-// window.onresize = function(event) {
-// 	console.log("x: " + window.innerWidth+"      y: " + window.innerHeight)
-//     if ($(this).width() < 800){
-// document.getElementById('carousel').removeClass('carousel').addClass('');}
-// }
-// }
-// resize()    
+const renderMenu = function() {
+		document.getElementById("header_first").insertAdjacentHTML('afterbegin',
+			`<div class="navbar-toggler">
+            <a class="burger_mobile" id="myBtn0"></a>
+        </div>
+        <div id="myModal0" class="modal">
+            <div class="modal-content">
+                <nav class="header_navigation" id="header_navigation">
+                    <span id="disable-scroll" class="close0 close-menu">
+                        <ul class="header_navigation_items">
+                            <a href="#works" class="modal-menu-nav">About us</a>
+                            <a href="#our-tours" class="modal-menu-nav">Our tours</a>
+                            <a href="#footer" class="modal-menu-nav">Contact us</a>
+                            <a href="#works" class="modal-menu-nav">Discount request</a>
+                            <a href="#works" class="modal-menu-nav">Hot deals</a>
+                        </ul>
+                        <div class="modal-social-media">
+                            <a class="btn-modal-social btn-outline-light btn-floating m-1"
+                                href="https://www.facebook.com/reneicanc" role="button" target="_blank"><i
+                                    class="fab fa-facebook-f" style="margin-left: 2px;"></i></a>
+                            <a class="btn-modal-social btn-outline-light btn-floating m-1"
+                                href="https://twitter.com/IvanFedak" role="button" target="_blank"><i
+                                    class="fab fa-twitter"></i></a>
+                            <a class="btn-modal-social btn-outline-light btn-floating m-1" href="google.com"
+                                role="button" target="_blank">
+                                <i class="fab fa-google"></i></a>
+                            <a class="btn-modal-social btn-outline-light btn-floating m-1"
+                                href="https://www.instagram.com/cruise_blog" role="button" target="_blank"><i
+                                    class="fab fa-instagram"></i></a>
+                            <a class="btn-modal-social btn-outline-light btn-floating m-1"
+                                href="https://www.linkedin.com/in/ivanfedak" role="button" target="_blank"><i
+                                    class="fab fa-linkedin-in"></i></a>
+                            <a class="btn-modal-social btn-outline-light btn-floating m-1"
+                                href="https://github.com/IvanFedak2" role="button" target="_blank"><i
+                                    class="fab fa-github"></i></a>
+                        </div>
+                    </span>
+                </nav>
+            </div>
+        </div>
+		`);
+	}
+    renderMenu()
---
+++ js/dynamic-international-tours.js
@@ -0,0 +1,302 @@
+// const renderInternational = function() {
+// 	const arr = [
+// 		{
+// 			classMain: "more-stuff-grid slide-right",
+//             imgSrc: "img/internationalTours/safari.jpg",
+//             imgAlt: "Safari",
+//             imgClass: "slide-in from-left image-fade-in image-fade-in-from-left",
+//             pClass: "slide-in from-right",
+//             pText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+//             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+//             and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+//             leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+//             with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+//             publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+// 		},
+//         {
+// 			classMain: "more-stuff-grid slide-left",
+//             imgSrc: "img/internationalTours/yellow.jpg",
+//             imgAlt: "Yellow Stone National Park",
+//             imgClass: "slide-in from-right image-fade-in",
+//             pClass: "slide-in from-left",
+//             pText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+//             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+//             and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+//             leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+//             with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+//             publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+// 		},
+//         {
+// 			classMain: "more-stuff-grid slide-right",
+//             imgSrc: "img/internationalTours/troll.jpg",
+//             imgAlt: "Norway Fjords",
+//             imgClass: "slide-in from-left image-fade-in image-fade-in-from-left",
+//             pClass: "slide-in from-right",
+//             pText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+//             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+//             and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+//             leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+//             with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+//             publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+// 		},
+//         {
+// 			classMain: "more-stuff-grid slide-left",
+//             imgSrc: "img/internationalTours/chichenitza.jpg",
+//             imgAlt: "Chichenitza, Mexico",
+//             imgClass: "slide-in from-right image-fade-in",
+//             pClass: "slide-in from-left",
+//             pText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+//             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+//             and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+//             leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+//             with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+//             publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+// 		},
+//         {
+// 			classMain: "more-stuff-grid slide-right",
+//             imgSrc: "img/internationalTours/beach1.jpg",
+//             imgAlt: "Beach adventure",
+//             imgClass: "slide-in from-left image-fade-in image-fade-in-from-left",
+//             pClass: "slide-in from-right",
+//             pText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+//             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+//             and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+//             leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+//             with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+//             publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+// 		},
+//         {
+// 			classMain: "more-stuff-grid slide-left",
+//             imgSrc: "img/internationalTours/golden.jpg",
+//             imgAlt: "Golden circle Island",
+//             imgClass: "slide-in from-right image-fade-in",
+//             pClass: "slide-in from-left",
+//             pText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+//             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+//             and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+//             leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+//             with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+//             publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+// 		},
+//         {
+// 			classMain: "more-stuff-grid slide-right",
+//             imgSrc: "img/internationalTours/faroe.jpg",
+//             imgAlt: "Faroe Islands",
+//             imgClass: "slide-in from-left image-fade-in image-fade-in-from-left",
+//             pClass: "slide-in from-right",
+//             pText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+//             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+//             and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+//             leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+//             with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+//             publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+// 		},
+//         {
+// 			classMain: "more-stuff-grid slide-left",
+//             imgSrc: "img/internationalTours/beaching.jpg",
+//             imgAlt: "beach",
+//             imgClass: "slide-in from-right image-fade-in",
+//             pClass: "slide-in from-left",
+//             pText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+//             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+//             and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+//             leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+//             with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+//             publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+// 		},
+//         {
+// 			classMain: "more-stuff-grid slide-right",
+//             imgSrc: "img/internationalTours/antarctica.jpg",
+//             imgAlt: "antarctica",
+//             imgClass: "slide-in from-left image-fade-in image-fade-in-from-left",
+//             pClass: "slide-in from-right",
+//             pText: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+//             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+//             and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+//             leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+//             with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+//             publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+// 		}
+// 	]
+// 	for(let i = 0; i <= arr.length; i++) {
+	
+// 		document.getElementById("home-more-stuff").insertAdjacentHTML('afterbegin',
+// 			`
+//             <div class="${arr[i].classMain}">
+                
+//                 <p class="${arr[i].pClass}">${arr[i].pText}</p>
+//                 <img src="${arr[i].imgSrc}" alt="${arr[i].imgAlt}" class="${arr[i].imgClass}"/>
+//             </div>
+// 		`);
+// 	}
+// }
+// renderInternational()
+
+
+
+
+document.getElementById("international-tours").insertAdjacentHTML('afterbegin',
+`<div class="home-about-wrapper">
+<div class="home-about">
+    <h2>International tours</h2>
+    <p>Book Tours & Travel Packages</p>
+    <div class="columns">
+        <div class="col fade-in">
+            <h3>Lorem, ipsum.</h3>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae
+                maiores fuga eos provident voluptas perferendis.
+            </p>
+        </div>
+        <div class="col fade-in">
+            <h3>Lorem, ipsum.</h3>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatibus minima quo beatae eius blanditiis officiis.
+            </p>
+        </div>
+        <div class="col fade-in">
+            <h3>Lorem, ipsum.</h3>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
+                quasi quis doloribus quia illum laudantium.
+            </p>
+        </div>
+    </div>
+</div>
+</div>
+<div class="home-more-stuff" id="home-more-stuff">
+
+
+<div class="more-stuff-grid slide-right">
+    <img src="img/internationalTours/antarctica.jpg" alt="antarctica" class="slide-in from-left image-fade-in image-fade-in-from-left" />
+    <p class="slide-in from-right">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+        with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+</div>
+
+
+
+<div class="more-stuff-grid slide-left">
+    <p class="slide-in from-left">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+        with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+    <img src="img/internationalTours/beaching.jpg" alt="Beach" class="slide-in from-right image-fade-in" />
+</div>
+
+
+<div class="more-stuff-grid slide-right">
+    <img src="img/internationalTours/faroe.jpg" alt="" class="slide-in from-left image-fade-in image-fade-in-from-left" />
+    <p class="slide-in from-right">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+        with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    </p>
+</div>
+
+
+
+<div class="more-stuff-grid slide-left">
+    <p class="slide-in from-left">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+        with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+
+    </p>
+    <img src="img/internationalTours/golden.jpg" alt="" class="slide-in from-right image-fade-in" />
+</div>
+
+
+
+<div class="more-stuff-grid slide-right">
+    <img src="img/internationalTours/beach1.jpg" alt="" class="slide-in from-left image-fade-in image-fade-in-from-left" />
+    <p class="slide-in from-right">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+        with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+
+    </p>
+</div>
+<div class="more-stuff-grid slide-left">
+    <p class="slide-in from-left">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+        with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+
+    </p>
+    <img src="img/internationalTours/chichenitza.jpg" alt="" class="slide-in from-right image-fade-in" />
+</div>
+
+
+
+
+<div class="more-stuff-grid slide-right">
+    <img src="img/internationalTours/troll.jpg" alt="" class="slide-in from-left image-fade-in image-fade-in-from-left" />
+    <p class="slide-in from-right">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+        with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+
+    </p>
+</div>
+
+<div class="more-stuff-grid slide-left">
+    <p class="slide-in from-left">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+        with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+
+    </p>
+    <img src="img/internationalTours/yellow.jpg" alt="" class="slide-in from-right image-fade-in" />
+</div>
+
+
+
+<div class="more-stuff-grid slide-right">
+    <img src="img/internationalTours/safari.jpg" alt="" class="slide-in from-left image-fade-in image-fade-in-from-left" />
+    <p class="slide-in from-right">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+        with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+
+    </p>
+</div>
+</div>`
+);
